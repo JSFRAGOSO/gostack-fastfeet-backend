74f5951fc89a4aac9b32fbf8b1e44a70181aa978
Fragoso <jonas.fragoso@sap.com>
2020-03-01 00:48:44 -0300

    Doing de Mail with background jobs, using Redis

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -2,7 +2,8 @@ import Recipient from '../models/Recipient';
 import Deliveryman from '../models/Deliveryman';
 import Order from '../models/Order';
 import File from '../models/File';
-import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import NewOrderMail from '../jobs/NewOrderMail';
 
 class OrderController {
     async index(req, res) {
@@ -52,22 +53,7 @@ class OrderController {
 
         const order = await Order.create(req.body);
 
-        await Mail.sendMail({
-            to: `${deliveryman.name} <${deliveryman.email}>`,
-            subject: `Nova entrega para ${recipient.name}`,
-            template: 'neworder',
-            context: {
-                deliverman: deliveryman.name,
-                recipient: recipient.name,
-                street: recipient.street,
-                number: recipient.number,
-                city: recipient.city,
-                state: recipient.state,
-                complement: recipient.complement,
-                zipcode: recipient.zip_code,
-                product: order.product,
-            },
-        });
+        Queue.add(NewOrderMail.key, { deliveryman, order, recipient });
 
         return res.json(order);
     }
